63a7aa4aab65103661aa0f995b6e1d49f874ef7d
nadeemhussain4462 <hoteleyenadeem@gmail.com>
2024-08-25 11:33:40 +0500

    JS chap-03

---
+++ app.js
@@ -0,0 +1,21 @@
+// Chapter No # 3 
+// Assignment no 2
+
+// Question no 1
+var age = 20;
+alert("I am " + age + " years old");
+
+// Question no 2
+var visit = 14;
+alert("You have visited this site " + visit + " times");
+
+// Question no 3
+var birthYear = 2003;
+document.write("My birth year is " + birthYear + "<br>");
+
+// Question no 4
+var visitorName = "John Doe";
+var productTitle = "T-Shirt";
+var quantity = 5;
+document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store");
+
