04cd5b28b66f3db10ec76196b6f0a6211aea0361
Nadeem Hussain <hoteleyenadeem@gmail.com>
2024-08-31 10:41:09 +0500

    js assignment 2

--- appp.js
+++ appp.js
@@ -108,12 +108,94 @@ var celsius = (fahrenheit - 32) * 5 / 9;
 document.write(fahrenheit + "°F is " + celsius + "°C" + "<br>");
 // Question no 7
 
-var priceItem1 = 10;
-var priceItem2 = 20;
+var priceItem1 = 650;
+var priceItem2 = 100;
 var quantityItem1 = 3;
-var quantityItem2 = 5;
-var shippingCharges = 5;
+var quantityItem2 = 7;
+var shippingCharges = 100;
 var totalCost = (priceItem1 * quantityItem1) + (priceItem2 * quantityItem2) + shippingCharges;
 document.write("Total cost is " + totalCost + "<br>");
+// Question no 8
+
+var totalMarks = 980;
+var obtainedMarks = 804;
+var percentage = (obtainedMarks / totalMarks) * 100;
+document.write("Percentage: " + percentage + "%" + "<br>");
+
+// Question no 9
+
+var dollar = 10;
+var riyal = 25;
+var pakistaniRupees = (dollar * 104.80) + (riyal * 28);
+document.write("Total currency in Pakistani Rupees: " + pakistaniRupees + "<br>");
+// Question no 10
+
+var num = 5;
+var result = ((num + 5) * 10) / 2;
+document.write("Result: " + result + "<br>");
+// Question no 11
+
+var currentYear = 2023;
+var birthYear = 2003;
+var age = currentYear - birthYear;
+document.write("Current Year: " + currentYear + "<br>");
+document.write("Birth Year: " + birthYear + "<br>");
+document.write("Age: " + age + "<br>");
+// Question no 12
+
+var radius = 20;
+var circumference = 2 * Math.PI * radius;
+var area = Math.PI * Math.pow(radius, 2);
+document.write("The circumference is " + circumference.toFixed(2) + "<br>");
+document.write("The area is " + area.toFixed(2) + "<br>");
+// Question no 13
+
+var favoriteSnack = "chocolate";
+var currentAge = 20;
+var maxAge = 65;
+var amountPerDay = 2;
+var yearsRemaining = maxAge - currentAge;
+var daysRemaining = yearsRemaining * 365;
+var totalSnacksRequired = daysRemaining * amountPerDay;
+document.write("You will need " + totalSnacksRequired + " " + favoriteSnack + " to last you until the ripe old age of " + maxAge + "<br>");
+// Chapter NO # 6-9 (MATH EXPRESSIONS)
+// Assignment no 2
+
+// Question no 1
+var a = 10;
+document.write("The value of a is: " + a + "<br>");
+var b = ++a;
+document.write("The value of ++a is: " + b + "<br>");
+var c = a++;
+document.write("The value of a++ is: " + c + "<br>");
+var d = --a;
+document.write("The value of --a is: " + d + "<br>");
+var e = a--;
+document.write("The value of a-- is: " + e + "<br>");
+// Question no 2
+var a = 2, b = 1;
+var result = --a - --b + ++b + b--;
+document.write("a is " + a + "<br>");
+document.write("b is " + b + "<br>");
+document.write("Result is " + result + "<br>");
+// Question no 3
+var name = prompt("Enter your name:");
+alert("Hello, " + name + "!");
+// Question no 5
+var num = prompt("Enter a number:");
+if (num !== null && num !== "") {
+    var number = parseInt(num);
+    if (!isNaN(number)) {
+        var multiplicationTable = "";
+        for (var i = 1; i <= 10; i++) {
+            multiplicationTable += num + " x " + i + " = " + (number * i) + "\n";
+        }
+        alert(multiplicationTable);
+    } else {
+        alert("Invalid input. Please enter a valid number.");
+    }
+} else {
+    alert("You did not enter a number.");
+}
 
 
